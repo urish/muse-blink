8e006f38dde3472389afdcaf63c79e17cb2f56c5
Uri Shaked <uri@salsa4fun.co.il>
2017-08-24 01:33:16 +0300

    feat: simple blink detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { MuseClient, channelNames } from 'muse-js';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private muse = new MuseClient();
+  connected = false;
+  leftBlinks: Observable<number>;
+  rightBlinks: Observable<number>;
+
+  constructor() {
+    this.muse = new MuseClient();
+    this.muse.connectionStatus.subscribe(newStatus => {
+      this.connected = newStatus;
+    });
+  }
+
+  async connect() {
+    await this.muse.connect();
+
+    const leftChannel = channelNames.indexOf('AF7');
+    const rightChannel = channelNames.indexOf('AF8');
+
+    this.leftBlinks = this.muse.eegReadings
+      .filter(r => r.electrode === leftChannel)
+      .map(r => Math.max(...r.samples.map(n => Math.abs(n))))
+      .filter(max => max > 500)
+      .switchMap(() =>
+        Observable.merge(
+          Observable.of(1),
+          Observable.timer(500).map(() => 0)
+        )
+      );
+
+    this.rightBlinks = this.muse.eegReadings
+      .filter(r => r.electrode === rightChannel)
+      .map(r => Math.max(...r.samples.map(n => Math.abs(n))))
+      .filter(max => max > 500)
+      .switchMap(() =>
+        Observable.merge(
+          Observable.of(1),
+          Observable.timer(500).map(() => 0)
+        )
+      );
+
+    this.muse.start();
+  }
+
+  disconnect() {
+    this.muse.disconnect();
+  }
 }
